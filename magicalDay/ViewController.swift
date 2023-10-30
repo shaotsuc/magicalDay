92e2eef09927655731539d1508cce15f439bf3cc
shaotsu <72166702+shaotsuc@users.noreply.github.com>
2023-10-30 21:03:49 +0200

    updated UI elements and function button.

--- magicalDay/ViewController.swift
+++ magicalDay/ViewController.swift
@@ -1,20 +1,21 @@
 //
 //  ViewController.swift
 //  Created by Shao-Tsu on 29/10/2023
-//  Updated by Shao-Tsu on 29/10/2023
+//  Updated by Shao-Tsu on 30/10/2023
 //  Project: magicalDay
-//  Verison: 1
+//  Verison: 2
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var lineToday: UIImageView!
+    
+    @IBAction func checkButton(_ sender: UIButton) {
+        
+        let lineArray = [ #imageLiteral(resourceName: "quote1"), #imageLiteral(resourceName: "quote2"), #imageLiteral(resourceName: "quote3"), #imageLiteral(resourceName: "quote4"), #imageLiteral(resourceName: "quote5")]
+        lineToday.image = lineArray.randomElement()
     }
-
-
 }
 
